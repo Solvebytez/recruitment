8ac888daf230d849879ad99d2394008990de0ab5
Solvebytez <githubsolvebytez@gmail.com>
2025-05-03 13:00:14 +0530

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -158,3 +158,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+
+
+window.addEventListener("load", function() {
+    if (window.location.hash) {
+      const target = document.querySelector(window.location.hash);
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  });
+
